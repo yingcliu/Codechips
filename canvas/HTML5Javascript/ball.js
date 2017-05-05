c24ec1ca90ad7a9de5b4e3d7af17384d7991e364
lyingc <lyingc@servyou.com.cn>
2017-05-05 18:03:49 +0800

    +

---
+++ canvas/HTML5Javascript/ball.js
@@ -0,0 +1,30 @@
+/**
+ * Created by lyingc on 2017/5/5.
+ */
+function Ball(radius, color) {
+    this.x = 0;
+    this.y = 0;
+    this.radius = radius || 40;
+    this.rotation = 0;
+    this.scaleX = 1;
+    this.scaleY = 1;
+    this.color = color || '#ffff00';
+    this.lineWidth = 1;
+}
+
+Ball.prototype.draw = function (context) {
+    context.save();
+    context.translate(this.x, this.y);
+    context.rotate(this.rotation);
+    context.scale(this.scaleX, this.scaleY);
+    context.lineWidth = this.lineWidth;
+    context.fillStyle = this.color;
+    context.beginPath();
+    context.arc(0, 0, this.radius, 0, (Math.PI*2), true);
+    context.closePath();
+    context.fill();
+    if(this.lineWidth > 0) {
+        context.stroke()
+    }
+    context.restore();
+};
