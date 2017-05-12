393919fb0f000bc56e4646b115c1065db4d9516b
lyingc <lyingc@servyou.com.cn>
2017-05-12 14:59:59 +0800

    默认的

--- canvas/HTML5Javascript/utils.js
+++ canvas/HTML5Javascript/utils.js
@@ -61,3 +61,37 @@ utils.captureTouch = function(element) {
     }, false);
     return touch;
 };
+utils.colorToRGB = function (color, alpha) {
+    if(typeof color === 'string' && color[0] === '#') {
+        color = window.parseInt(color.slice(1), 16);
+    }
+    alpha = (alpha === undefined) ? 1 : alpha;
+
+    var r = color >> 16 & 0xff,
+        g = color >> 8 & 0xff,
+        b = color & 0xff,
+        a = (alpha < 0) ? 0 : ((alpha > 1) ? 1 : alpha);
+
+    if(a === 1) {
+        return 'rgb(' + r + ',' + g + ',' + b + ')';
+    }
+    return 'rgba(' + r + ',' + g + ',' + 'b' + ',' + a + ')';
+}
+
+utils.parseColor =  function (color, toNumber) {
+    if(typeof toNumber === true) {
+        if(typeof color === 'number') {
+            return (color | 0);
+        }
+        if(typeof color === 'string' && color[0] === '#') {
+            color = color.slice(1);
+        }
+        return window.parseInt(color, 16)
+    } else {
+        if(typeof color === 'number') {
+            //?????
+            color = '#' + ('00000' + (color | 0).toString(16)).substr(-6)
+        }
+        return color;
+    }
+}
